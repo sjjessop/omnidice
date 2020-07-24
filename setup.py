45cd4434cfea62187254c80ce9f777d0979a8254
Steve Jessop <68118527+sjjessop@users.noreply.github.com>
2020-07-24 17:04:09 +0100

    Release 1.0.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name='omnidice',
-    version='0.0.1',
+    version='1.0.0',
     python_requires='>=3.7.3',
     packages=setuptools.find_packages(),
     install_requires=[
