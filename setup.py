5769d48bb9fc96383d9a03413468ab9c6de4879a
Steve Jessop <68118527+sjjessop@users.noreply.github.com>
2023-03-04 17:52:34 +0000

    Pin importlib-metadata for the benefit of flake8

--- setup.py
+++ setup.py
@@ -20,6 +20,10 @@ setuptools.setup(
             'sphinx>=3.3.0',
             'wheel',
         ],
+        'dev:python_version<"3.8"': [
+            # https://github.com/python/importlib_metadata/issues/406
+            'importlib-metadata<5'
+        ],
         'pd': ['pandas'],
     },
     package_data={
