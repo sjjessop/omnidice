6c694e39334496d68e1a8889887d4c3eefcc4d3e
Steve Jessop <68118527+sjjessop@users.noreply.github.com>
2025-04-13 21:35:59 +0100

    Release 1.4.0

--- docn/conf.py
+++ docn/conf.py
@@ -23,7 +23,7 @@ copyright = '2020, Steve Jessop'
 author = 'Steve Jessop'
 
 # The full version, including alpha/beta/rc tags
-release = '1.3.0'
+release = '1.4.0'
 
 
 # -- General configuration ---------------------------------------------------
